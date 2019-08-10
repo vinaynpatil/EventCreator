a3749a81818d0f9af948f61ce2064b9129a2e634
vinaynpatil <reachvinaynp@gmail.com>
2019-08-10 14:16:38 -0500

    Adding services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { EventsAppComponent } from './events-app.component';
 
-import {EventListComponent} from './events/events-list.component'
+import { EventListComponent } from './events/events-list.component'
 
-import {EventThumbnailComponent} from './events/event-thumbnail.component'
+import { EventThumbnailComponent } from './events/event-thumbnail.component'
 
-import {NavBarComponent} from './nav/navbar.component'
+import { NavBarComponent } from './nav/navbar.component'
+
+import { EventService } from './events/shared/event.service'
+
+import { ToastrService } from './common/toastr.service'
 
 @NgModule({
   declarations: [
@@ -19,7 +23,7 @@ import {NavBarComponent} from './nav/navbar.component'
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [EventService, ToastrService],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
